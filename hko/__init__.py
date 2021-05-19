94c3ab67e5077284e6e79bdc521cf6b6c82c2a8a
MisterCommand <anthonyhou04@gmail.com>
2021-05-19 17:21:41 +0800

    Add rhrread

---
+++ hko/__init__.py
@@ -0,0 +1,26 @@
+__version__ = '0.1.0'
+
+import json
+import logging
+from aiohttp import ClientSession, ClientResponse
+from typing import Dict
+
+_LOGGER = logging.getLogger(__name__)
+
+class HKO:
+    """Main class to communicate with the HKO API."""
+
+    def __init__(self, session: ClientSession):
+        self._session = session
+
+    async def _get_data(self, url:str) -> Dict:
+        """Retreive data from HKO API"""
+        async with self._session.get(url) as response:
+            if response.status != 200:
+                error_text = json.loads(await response.text())
+            return await response.json()
+
+    async def get_rhrread(self) -> Dict:
+        """Retreive rhrread data from HKO API"""
+        data = await self._get_data("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en")
+        return data
