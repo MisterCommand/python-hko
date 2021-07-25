a55836991ff55c571f58681d228e65c8e790ace8
MisterCommand <anthonyhou04@gmail.com>
2021-07-25 22:49:13 +0800

    Update method

--- hko/__init__.py
+++ hko/__init__.py
@@ -13,19 +13,12 @@ class HKO:
     def __init__(self, session: ClientSession):
         self._session = session
 
-    async def _get_data(self, url:str) -> Dict:
+    async def _request(self, url:str) -> Dict:
         """Retreive data from HKO API"""
         async with self._session.get(url) as response:
-            if response.status != 200:
-                error_text = json.loads(await response.text())
             return await response.json()
 
-    async def get_rhrread(self) -> Dict:
-        """Retreive rhrread data from HKO API"""
-        data = await self._get_data("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en")
-        return data
-
-    async def get_fnd(self) -> Dict:
-        """Retreive fnd data from HKO API"""
-        data = await self._get_data("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en")
+    async def get(self, type, lang = "en") -> Dict:
+        """Get data"""
+        data = await self._request(f"https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType={type}&lang={lang}")
         return data
